5c8cd1fefd3d18f0244830731be289b3f01595bf
Eric Klaus <ericklaus16@gmail.com>
2024-09-27 22:31:08 -0300

    modified

--- src/main.tsx
+++ src/main.tsx
@@ -2,13 +2,10 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
-import { Theme } from '@radix-ui/themes'
 import '@radix-ui/themes/styles.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <Theme appearance='light'>
       <App />
-    </Theme>
   </StrictMode>,
 )
