220e2a6964063ccee38fe527e5b97f7a59e79892
Eric Klaus <ericklaus16@gmail.com>
2024-09-27 22:27:19 -0300

    Add Vite environment declaration file, index.css, postcss.config.js, tsconfig.json, TaskType.ts, vite.config.ts, tailwind.config.js, .gitignore, main.tsx, index.html, tsconfig.node.json, tsconfig.app.json, App.css, eslint.config.js, package.json, and vite.svg

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,4 @@
 import { useEffect, useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import { Inset, Strong, Text, Box, Card } from '@radix-ui/themes'
 import { TaskType } from './types/TaskType'
